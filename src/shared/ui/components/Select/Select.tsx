01dfb8aed9989cdffe35eeb0bfef2dcecf41b8f6
morozov312 <alexander.morozov312@gmail.com>
2023-12-06 18:29:38 +0530

    feat: [CHAT-2] create settings form

--- src/shared/ui/components/Select/Select.tsx
+++ src/shared/ui/components/Select/Select.tsx
@@ -1,16 +1,35 @@
+'use client';
 import { DetailedHTMLProps, SelectHTMLAttributes } from 'react';
+import { useController, UseControllerProps } from 'react-hook-form';
 
 interface IProps
   extends DetailedHTMLProps<
-    SelectHTMLAttributes<HTMLSelectElement>,
-    HTMLSelectElement
-  > {}
+      SelectHTMLAttributes<HTMLSelectElement>,
+      HTMLSelectElement
+    >,
+    UseControllerProps {
+  defaultValue?: string;
+  name: string;
+}
 
-export const Select = ({ children, ...props }: IProps) => {
+export const Select = ({
+  children,
+  name,
+  defaultValue,
+  control,
+  ...props
+}: IProps) => {
+  const { field } = useController({
+    name,
+    defaultValue,
+    control,
+  });
+
   return (
     <select
+      {...field}
       {...props}
-      className='min-w-[200px] rounded-xl bg-gray-40 p-2 outline-0'
+      className='min-w-[100px] rounded-xl bg-gray-40 p-2 outline-0 transition-all hover:bg-[rgba(86,88,105,.7)]'
     >
       {children}
     </select>
--- src/widgets/navbar/ui/Navbar.tsx
+++ src/widgets/navbar/ui/Navbar.tsx
@@ -4,8 +4,11 @@ import { ControlsSection } from '@/features';
 
 export const Navbar = () => {
   return (
-    <nav className='flex justify-between px-4 py-6'>
-      <Logo />
+    <nav className='flex items-center justify-between px-4 py-6'>
+      <div className='flex cursor-pointer items-center gap-4 font-bold'>
+        <Logo />
+        <h1>{'LLM Chat Clone'}</h1>
+      </div>
       <ControlsSection />
     </nav>
   );
